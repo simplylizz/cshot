f42f513ddaca75433675cb5ff04a729cdecb67da
Anton Yanchenko <simplylizz@gmail.com>
2017-03-25 16:32:22 +0300

    Hook on lock event

--- cshot/main.m
+++ cshot/main.m
@@ -8,10 +8,40 @@
 
 #import <Foundation/Foundation.h>
 
+
+@interface LockWatcher : NSObject
+- (id) init;
+- (void)screenUnlocked;
+@end
+
+
+@implementation LockWatcher
+- (id) init {
+    NSDistributedNotificationCenter *center = [NSDistributedNotificationCenter defaultCenter];
+    [center addObserver:self
+               selector:@selector(screenUnlocked)
+                   name:@"com.apple.screenIsUnlocked"
+                 object:nil
+     ];
+
+    return self;
+}
+
+- (void)screenUnlocked {
+    NSLog(@"Screen is unlocked!");
+}
+@end
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hi there!!");
+
+        // NOTE: without variable program is failing with exception... maybe there is some optimizations?
+        LockWatcher *lw = [[LockWatcher alloc]init];
+        [[NSRunLoop currentRunLoop] run];
+
+        NSLog(@"How ar u?");
     }
     return 0;
 }
