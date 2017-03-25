85aecc3a1765b2a850079eedf752ab91f861e42f
Anton Yanchenko <simplylizz@gmail.com>
2017-03-25 15:47:44 +0300

    Initial Commit

---
+++ cshot/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cshot
+//
+//  Created by AY on 25/03/2017.
+//  Copyright © 2017 ZetLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
